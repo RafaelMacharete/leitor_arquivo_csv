ba86fb8bdad4f6d283e08a57e4cc1091c02f1fa7
RafaelMacharete <rafaelmacharete00@gmail.com>
2024-12-05 21:37:38 -0300

    aplicando práticas clean code

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Este código faz uma automação de leitura de um arquivo base .csv de um relatório sobre esteiras
+# Onde eu leio o arquivo base .csv (ESP8266_Receiver - Sheet1.csv)
+# Adiciono em um arquivo .csv (relatorio_esteiras_com_problemas.csv) todas as esteiras que estão abaixo do nível crítico, exibindo também o horário e data
+# Envio os dados das esteiras que estão abaixo do nível crítico em um email
+# Envio os dados das esteiras que estão abaixo do nível crítico em um arquivo .xlsx (relatorio_esteiras_com_problemas.xlsx)
+
 import csv, smtplib, os, openpyxl
 import pandas as pd
 from colorama import Fore, Back, Style
@@ -19,23 +25,26 @@ def send_email(subject, body, sender, recipients, password):
 
 relatorio_csv = "relatorio_esteiras_com_problemas.csv"
 
-with open(relatorio_csv, 'w', newline='', encoding='utf-8') as arquivo_relatorio_csv:
-                spamwriter = csv.writer(arquivo_relatorio_csv)
-                spamwriter.writerow(['Date', 'Time', 'esteira0', 'esteira1', 'esteira2'])
+# Cria o arquivo base para o relatório final 
+with open(relatorio_csv, 'w', newline='', encoding='utf-8') as arquivo_relatorio_csv: 
+    spamwriter = csv.writer(arquivo_relatorio_csv)
+    spamwriter.writerow(['Date', 'Time', 'esteira0', 'esteira1', 'esteira2'])
                 
-# Ler o arquvio .csv
+# Lê o arquivo base da automação .csv
 with open('ESP8266_Receiver - Sheet1.csv', newline='') as arquivo_csv:
     conteudo_arquivo_csv = csv.reader(arquivo_csv, delimiter=',')
-    next(conteudo_arquivo_csv)  # Ignorar primeira linha do arquivo_csv
+    next(conteudo_arquivo_csv)
     body = []
-    
-    for linha_arquivo_csv in conteudo_arquivo_csv:  # Percorre cada linha da lista
+
+    # Percorre cada linha da lista
+    for linha_arquivo_csv in conteudo_arquivo_csv:
 
         # Verifica os níveis críticos de cada esteira e adiciona ao corpo do e-mail
         if int(linha_arquivo_csv[2]) < 5:  # esteira 0
             body.append(f"A esteira 0 no dia {linha_arquivo_csv[0]} no tempo: {linha_arquivo_csv[1]}\nestava abaixo do nível crítico!!\n")
             print(f"{Fore.RED}A esteira 0 no dia {linha_arquivo_csv[0]} no tempo: {linha_arquivo_csv[1]}\nestava abaixo do nível crítico!!\n")
 
+            # Código base para a criação do arquivo base do relatorio
             with open(relatorio_csv, 'a', newline='', encoding='utf-8') as arquivo_relatorio_csv:
                 spamwriter = csv.writer(arquivo_relatorio_csv)
                 spamwriter.writerow([linha_arquivo_csv[0], linha_arquivo_csv[1], linha_arquivo_csv[2], '-', '-'])
@@ -44,6 +53,7 @@ with open('ESP8266_Receiver - Sheet1.csv', newline='') as arquivo_csv:
             body.append(f"A esteira 1 no dia {linha_arquivo_csv[0]} no tempo: {linha_arquivo_csv[1]}\nestava abaixo do nível crítico!!\n")
             print(f"{Fore.BLUE}A esteira 1 no dia {linha_arquivo_csv[0]} no tempo: {linha_arquivo_csv[1]}\nestava abaixo do nível crítico!!\n")
 
+            # Código base para a criação do arquivo base do relatorio
             with open(relatorio_csv, 'a', newline='', encoding='utf-8') as arquivo_relatorio_csv:
                 spamwriter = csv.writer(arquivo_relatorio_csv)
                 spamwriter.writerow([linha_arquivo_csv[0], linha_arquivo_csv[1], '-', linha_arquivo_csv[3], '-'])
@@ -52,6 +62,7 @@ with open('ESP8266_Receiver - Sheet1.csv', newline='') as arquivo_csv:
             body.append(f"A esteira 2 no dia {linha_arquivo_csv[0]} no tempo: {linha_arquivo_csv[1]}\nestava abaixo do nível crítico!!\n")
             print(f"{Fore.LIGHTYELLOW_EX}A esteira 2 no dia {linha_arquivo_csv[0]} no tempo: {linha_arquivo_csv[1]}\nestava abaixo do nível crítico!!\n")
 
+            # Código base para a criação do arquivo base do relatorio
             with open(relatorio_csv, 'a', newline='', encoding='utf-8') as arquivo_relatorio_csv:
                 spamwriter = csv.writer(arquivo_relatorio_csv)
                 spamwriter.writerow([linha_arquivo_csv[0], linha_arquivo_csv[1], '-', '-', linha_arquivo_csv[4]])
@@ -63,11 +74,11 @@ recipients = ["", ""] # Coloque o email para quem será enviado
 
 send_email(subject, body, sender, recipients, password)
 
-# Transformar o arquivo "relatorio_esteiras_com_problemas.csv" em um .xlsx
+# Lê o arquivo "relatorio_esteiras_com_problemas.csv" em um .xlsx
 novo_conjunto_dados = pd.read_csv(relatorio_csv)
 
-# Salva como arquivo Excel com extensão correta
+# Salva o arquivo lido como arquivo Excel
 relatorio_excel = "relatorio_esteiras_com_problemas.xlsx"
 novo_conjunto_dados.to_excel(relatorio_excel, index=False)
 
-print(f"Relatório salvo como: {relatorio_excel}")
+print(f"{Fore.GREEN}\nRelatório salvo como: {relatorio_excel}")
